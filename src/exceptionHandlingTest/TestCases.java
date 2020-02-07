5c330d0344931533b1ddbd3318ce96bdaa453c87
Arseniy Gromovoy <arseniygromovoy@gmail.com>
2020-02-07 15:35:06 +0200

    Tests rewritten to directly expect exceptions.

--- src/exceptionHandlingTest/TestCases.java
+++ src/exceptionHandlingTest/TestCases.java
@@ -1,17 +1,21 @@
 package exceptionHandlingTest;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
 
 public class TestCases {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Test
     public void testCase1() {
         int n = 3;
         int p = 5;
-        String expected = "243";
-        String actual = PowerCalculator.getMessageOnPower(n, p);
+        long expected = 243;
+        long actual = PowerCalculator.myPow(n, p);
 
         assertEquals(expected, actual);
     }
@@ -20,39 +24,42 @@ public class TestCases {
     public void testCase2() {
         int n = 2;
         int p = 4;
-        String expected = "16";
-        String actual = PowerCalculator.getMessageOnPower(n, p);
+        long expected = 16;
+        long actual = PowerCalculator.myPow(n, p);
 
         assertEquals(expected, actual);
     }
 
     @Test
     public void testCase3() {
+        thrown.expect(java.lang.ArithmeticException.class);
+        thrown.expectMessage("n and p should not be zero at the same time");
+
         int n = 0;
         int p = 0;
-        String expected = "java.lang.Exception: n and p should not be zero at the same time";
-        String actual = PowerCalculator.getMessageOnPower(n, p);
 
-        assertEquals(expected, actual);
+        PowerCalculator.myPow(n, p);
     }
 
     @Test
     public void testCase4() {
+        thrown.expect(java.lang.ArithmeticException.class);
+        thrown.expectMessage("n or p should not be negative");
+
         int n = -1;
         int p = -2;
-        String expected = "java.lang.Exception: n and p should not be negative";
-        String actual = PowerCalculator.getMessageOnPower(n, p);
 
-        assertEquals(expected, actual);
+        PowerCalculator.myPow(n, p);
     }
 
     @Test
     public void testCase5() {
+        thrown.expect(java.lang.ArithmeticException.class);
+        thrown.expectMessage("n or p should not be negative");
+
         int n = -1;
         int p = 3;
-        String expected = "java.lang.Exception: n and p should not be negative";
-        String actual = PowerCalculator.getMessageOnPower(n, p);
 
-        assertEquals(expected, actual);
+        PowerCalculator.myPow(n, p);
     }
 }
