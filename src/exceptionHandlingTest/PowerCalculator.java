e20e22a8bbccc7d42cd0e73a3b216696bb341656
Arseniy Gromovoy <arseniygromovoy@gmail.com>
2020-02-07 15:34:58 +0200

    Unnecessary string wrapper method removed

--- src/exceptionHandlingTest/PowerCalculator.java
+++ src/exceptionHandlingTest/PowerCalculator.java
@@ -2,12 +2,12 @@ package exceptionHandlingTest;
 
 public class PowerCalculator {
 
-    private static long myPow(int n, int p) throws Exception {
+    public static long myPow(int n, int p) throws ArithmeticException {
         if (n < 0 || p < 0) {
-            throw new Exception("n and p should not be negative");
+            throw new ArithmeticException("n or p should not be negative");
         }
         if (n == 0 && p == 0) {
-            throw new Exception("n and p should not be zero at the same time");
+            throw new ArithmeticException("n and p should not be zero at the same time");
         }
         if (n == 0) {
             return 0;
@@ -28,14 +28,5 @@ public class PowerCalculator {
             }
         }
         return result;
-    }
-    public static String getMessageOnPower(int n, int p) {
-        try {
-            long result = myPow(n, p);
-            return Long.toString(result);
-        }
-        catch (Exception e) {
-            return e.toString();
-        }
     }
 }
